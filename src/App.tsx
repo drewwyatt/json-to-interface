52c306f25d212784ea17c968e3f96419147bff87
Drew Wyatt <drew.j.wyatt@gmail.com>
2018-04-01 17:23:44 -0500

    added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import JSONField from './components/json-field';
-import './App.css';
+import * as React from 'react'
+import JSONField from './components/json-field'
+import './App.css'
 
-const logo = require('./logo.svg');
+const logo = require('./logo.svg')
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <JSONField />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <JSONField />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/components/json-field.tsx
+++ src/components/json-field.tsx
@@ -1,62 +1,72 @@
-import * as React from 'react';
+import * as React from 'react'
 
 interface ParseStatus {
-    code: ParseStatusCode;
-    message: string;
-  }
-  
+    code: ParseStatusCode
+    message: string
+}
+
 enum ParseStatusCode {
     notParsed,
     ok,
-    error
-  }
+    error,
+}
 
 interface Props {}
 
 interface State {
-    json: string;
-    parseStatus: ParseStatus;
+    json: string
+    parseStatus: ParseStatus
 }
 
 class JSONField extends React.Component<Props, State> {
     constructor(props: Props) {
-      super(props);
-      this.state = {
-        json: '',
-        parseStatus: {
-          code: ParseStatusCode.notParsed,
-          message: '',
+        super(props)
+        this.state = {
+            json: '',
+            parseStatus: {
+                code: ParseStatusCode.notParsed,
+                message: '',
+            },
         }
-      };
     }
     render() {
-      const { json, parseStatus: { code, message } } = this.state;
-      return (
-        <>
-          <textarea 
-            style={{ 
-              borderColor: code === ParseStatusCode.error ? 'red' : 'black' }} 
-            onChange={this.onJSONUpdate} 
-            value={json} 
-          />
-          <p style={{ color: 'red' }}>{message}</p>
-        </>
-      );
+        const { json, parseStatus: { code, message } } = this.state
+        return (
+            <>
+                <textarea
+                    style={{
+                        borderColor:
+                            code === ParseStatusCode.error ? 'red' : 'black',
+                    }}
+                    onChange={this.onJSONUpdate}
+                    value={json}
+                />
+                <p style={{ color: 'red' }}>{message}</p>
+            </>
+        )
     }
-  
-    private onJSONUpdate = ({ target: { value: json } }: React.ChangeEvent<HTMLTextAreaElement>) => {
-      this.setState({ json }, this.tryParseJSON);
+
+    private onJSONUpdate = ({
+        target: { value: json },
+    }: React.ChangeEvent<HTMLTextAreaElement>) => {
+        this.setState({ json }, this.tryParseJSON)
     }
-  
+
     private tryParseJSON = () => {
-      try {
-        JSON.parse(this.state.json);
-        this.setState({ 
-          parseStatus: { code: ParseStatusCode.ok, message: '' } });
-      } catch (err) {
-        this.setState({ parseStatus: { code: ParseStatusCode.error, message: (err as Error).message } });
-      }
+        try {
+            JSON.parse(this.state.json)
+            this.setState({
+                parseStatus: { code: ParseStatusCode.ok, message: '' },
+            })
+        } catch (err) {
+            this.setState({
+                parseStatus: {
+                    code: ParseStatusCode.error,
+                    message: (err as Error).message,
+                },
+            })
+        }
     }
-  }
-  
-export default JSONField;
+}
+
+export default JSONField
